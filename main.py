b6e4473bb9bec7fb2b45c7c53b606445af9c77cc
Delouya1 <delouya1@gmail.com>
2023-03-27 18:42:25 +0300

    model creation, convolutional and pooling layers, dense layer and output layer

--- main.py
+++ main.py
@@ -6,8 +6,37 @@ import cv2 as cv
 from tensorflow.python.keras import layers, models
 import matplotlib.pyplot as plt
 
-(training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-training_images, test_images = training_images / 255.0, test_images / 255.0
+(training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data() # load the data
+training_images, test_images = training_images / 255.0, test_images / 255.0  # normalize the data
 
-class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']  # the names of the classes
+
+for i in range(16):
+    plt.subplot(4, 4, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+plt.show()
+
+training_images = training_images[:20000]  # cut down the data to make training faster
+training_labels = training_labels[:20000]
+test_images = test_images[:4000]
+test_labels = test_labels[:4000]
+
+model = models.Sequential()  # create the model
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))  # add the first convolutional layer
+model.add(layers.MaxPooling2D((2, 2)))  # add the first pooling layer
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # add the second convolutional layer
+model.add(layers.MaxPooling2D((2, 2)))  # add the second pooling layer
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # add the third convolutional layer
+model.add(layers.Flatten())  # flatten the output of the convolutional layers
+model.add(layers.Dense(64, activation='relu'))  # add a dense layer
+model.add(layers.Dense(10, activation='softmax'))  # add the output layer
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])  # compile the model
+
+model.fit(training_images, training_labels, epochs=10, validation_data=(test_images, test_labels))  # train the model
+
 
