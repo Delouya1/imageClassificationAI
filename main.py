2082d60c60e49dabd2174d8012cda550c378d6f6
Delouya1 <delouya1@gmail.com>
2023-03-27 19:51:05 +0300

    trained and save the model

--- main.py
+++ main.py
@@ -20,31 +20,41 @@ for i in range(16):
 
 plt.show()
 
-training_images = training_images[:20000]  # cut down the data to make training faster
-training_labels = training_labels[:20000]
-test_images = test_images[:4000]
-test_labels = test_labels[:4000]
+# training_images = training_images[:20000]  # cut down the data to make training faster
+# training_labels = training_labels[:20000]
+# test_images = test_images[:4000]
+# test_labels = test_labels[:4000]
 
-model = models.Sequential()  # create the model
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))  # add the first convolutional layer
-model.add(layers.MaxPooling2D((2, 2)))  # add the first pooling layer
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # add the second convolutional layer
-model.add(layers.MaxPooling2D((2, 2)))  # add the second pooling layer
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # add the third convolutional layer
-model.add(layers.Flatten())  # flatten the output of the convolutional layers
-model.add(layers.Dense(64, activation='relu'))  # add a dense layer
-model.add(layers.Dense(10, activation='softmax'))  # add the output layer
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])  # compile the model
-
-model.fit(training_images, training_labels, epochs=10, validation_data=(test_images, test_labels))  # train the model
-
-loss, accuracy = model.evaluate(test_images, test_labels)  # evaluate the model
-print("Loss: ", loss)
-print("Accuracy: ", accuracy)
-
-model.save('model.h5')  # save the mode
+# model = models.Sequential()  # create the model
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))  # add the first convolutional layer
+# model.add(layers.MaxPooling2D((2, 2)))  # add the first pooling layer
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # add the second convolutional layer
+# model.add(layers.MaxPooling2D((2, 2)))  # add the second pooling layer
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # add the third convolutional layer
+# model.add(layers.Flatten())  # flatten the output of the convolutional layers
+# model.add(layers.Dense(64, activation='relu'))  # add a dense layer
+# model.add(layers.Dense(10, activation='softmax'))  # add the output layer
+#
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])  # compile the model
+#
+# model.fit(training_images, training_labels, epochs=10, validation_data=(test_images, test_labels))  # train the model
+#
+# loss, accuracy = model.evaluate(test_images, test_labels)  # evaluate the model
+# print("Loss: ", loss)
+# print("Accuracy: ", accuracy)
+#
+# model.save('model.h5')  # save the mode
 model = tf.keras.models.load_model('model.h5')  # load the model
+
+img = cv.imread('/Users/delouya.thefist/PycharmProjects/imageClassificationAI/ai training pics/horse.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # convert the image to RGB
+img = cv.resize(img, (32, 32))  # resize the image to 32x32
+
+plt.imshow(img, cmap=plt.cm.binary)  # show the image
+
+prediction = model.predict(np.array([img]))  # predict the class of the image
+index = np.argmax(prediction)  # get the index of the class with the highest probability
+print("Prediction is:", class_names[index])  # print the prediction
 
 
 
